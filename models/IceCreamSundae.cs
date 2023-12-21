a24c2e5c5ce1add4a5b1d12540daed9a425e4669
Garrett.Rathke <garrett.rathke@gmail.com>
2023-12-21 14:16:46 -0600

    moved data file to a data folder, added IceCreamSundae model class

---
+++ models/IceCreamSundae.cs
@@ -0,0 +1,9 @@
+namespace CountTheVariations.Models
+{
+    public class IceCreamSundae 
+    {
+        public string FlavorOne { get; set; }
+        public string FlavorTwo { get; set; }
+        public string FlavorThree { get; set; }
+    }
+}
